78335c91317369a8cb483ca762b1184b7c2d91d6
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-12-11 21:12:27 +0700

    fix: tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false,
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

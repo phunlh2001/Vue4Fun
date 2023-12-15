6f99cbc530fc47cae3ed5941c21573b59ede8de8
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-12-15 18:35:28 +0700

    feat: hero page

---
+++ src/components/Slide.vue
@@ -0,0 +1,91 @@
+<script setup>
+defineProps({
+  subTitle: String,
+})
+</script>
+
+<template>
+  <div class="h-screen w-full bg-gray-900 relative">
+    <div
+      class="w-[525px] h-[580px] absolute top-[115px] right-[250px] bg-cover bg-no-repeat"
+      style="background-image: url('/src/assets/right-image-hero-1.png')"
+    ></div>
+    <div
+      class="w-full h-full bg-no-repeat bg-cover flex gap-[120px] pl-[50px]"
+      style="
+        background-image: url('/src/assets/background-element-hero-section-1-1.png');
+      "
+    >
+      <!-- icon start -->
+      <div class="h-full w-max flex flex-col gap-[20px] justify-center">
+        <div
+          v-motion
+          :enter="{
+            scale: 1,
+          }"
+          :hovered="{
+            scale: 1.1,
+          }"
+          class="social"
+        >
+          <font-awesome-icon :icon="['fab', 'facebook']" />
+        </div>
+        <div
+          v-motion
+          :enter="{
+            scale: 1,
+          }"
+          :hovered="{
+            scale: 1.1,
+          }"
+          class="social"
+        >
+          <font-awesome-icon :icon="['fab', 'twitter']" />
+        </div>
+        <div
+          v-motion
+          :enter="{
+            scale: 1,
+          }"
+          :hovered="{
+            scale: 1.1,
+          }"
+          class="social"
+        >
+          <font-awesome-icon :icon="['fab', 'instagram']" />
+        </div>
+      </div>
+      <!-- icon end -->
+
+      <!-- title start -->
+      <div class="flex h-full w-[475px] flex-col gap-[20px] pt-[150px]">
+        <h1 class="text-white text-7xl whitespace-normal font-bold">
+          We Provide Cyber <br />
+          Solutions For Your Security.
+        </h1>
+        <p class="text-xl text-gray-400">{{ subTitle }}</p>
+        <!-- button start -->
+        <div class="w-full h-[57px] flex gap-[20px]">
+          <button
+            v-motion
+            :enter="{ y: 0 }"
+            :hovered="{ y: -4 }"
+            class="button outline-none bg-blue-500"
+          >
+            Get Started
+          </button>
+          <button
+            v-motion
+            :enter="{ y: 0 }"
+            :hovered="{ y: -4 }"
+            class="button outline-none border-[0.5px] border-gray-500"
+          >
+            Contact Us
+          </button>
+        </div>
+        <!-- button end -->
+      </div>
+      <!-- title end -->
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-createApp(App).mount('#app')
+import { MotionPlugin } from '@vueuse/motion'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(faTwitter, faFacebook, faInstagram)
+
+createApp(App)
+  .use(MotionPlugin)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
